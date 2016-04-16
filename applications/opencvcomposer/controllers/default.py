6864caeb70b0243e4987668c68e10bef17a0d29b
Roberto Perez <roberto@medallia.com>
2016-04-15 21:13:04 -0700

    Pruning old images

--- applications/opencvcomposer/controllers/default.py
+++ applications/opencvcomposer/controllers/default.py
@@ -5,63 +5,100 @@ import json
 import os
 import StringIO
 import uuid
+import datetime
 from PIL import Image
 from PIL import ImageEnhance
 
-def download():
-    return response.download(request,db)
+#--------------------------------------
+# Endpoints
+#--------------------------------------
 
-def link():
-    return response.download(request,db,attachment=False)
-
-def delete():
-    filename = request.args(0)
-    db(db.image.image==filename).delete()
-    redirect(URL('index'))
-
+# Endpoint: GET /
+# Response: HTML
+# Returns the index view, which presents the user
+# with the menu to start uploading images. It connects to FB
+# to allow the user pick from a gallery image.
 def index():
     return dict()
 
+# Endpoint: GET /image?fileId={file_id}
+# Response: file
+# Downloads an image that was previously uploaded. It receives the
+# id of the image. It will throw a 404 if the image is not found.
 def image():
     fileId = request.args(0)
-    fileName = db(db.image.title==fileId).select().first().image
+    record = db(db.image.title==fileId).select().first()
+    if not record:
+        raise HTTP(404, "Unknown fileId")    
+    fileName = record.image
     response.headers['Content-Type'] = "image/x-jpg"
     response.headers['Content-disposition'] = 'attachment; filename=image.jpg'
     path = os.path.join(request.folder, 'uploads', fileName)
     res = response.stream(open(path, "rb"), chunk_size=4096)
     return res
 
-def storeImage(file):
-    try:
-        fileId = str(uuid.uuid5(uuid.NAMESPACE_DNS, 'got06.com'))
-        filename = db.image.image.store(file, fileId + ".jpg")
-        path = os.path.join(request.folder, 'uploads', filename)
-        image = generateImage(open(path, 'rb'), 202, 0.15, 0.97, True)
-        image.save(path)
-        id = db.image.insert(image=filename,title=fileId)
-        return response.json({'status': 0, 'file_id': fileId})
-    except ValueError as err:
-        return response.json({'status': 1, 'message': str(err)})
-
+# Endpoint: POST /upload
+# Body: {
+#   image_file: file
+# }
+# Response: JSON {
+#   status: 0 (if everything went right) or 1 (if there was an 
+#      error
+#   file_id: string (identifier of the file, that can be used
+#      at /image?fileId={file_id})
+#   message: string (explanation of the error)
+# }
+# Uploads the given file and runs the face detection algorithm
+# over it, saving the final composed image to the database.
+# If everything goes right, it returns the file id.
+# otherwise it returns an error message.
 def upload():
     image_form = FORM(
 		INPUT(_name='image_file',_type='file')
 		)
 
     if image_form.accepts(request.vars,formname='image_form'):
-        return storeImage(image_form.vars.image_file.file)
+        return _storeImage(image_form.vars.image_file.file)
 
     raise HTTP(400, "Missing file parameter")
 
+# Endpoint GET /loadFromUrl?url={url}
+# Response: JSON {
+#   status: 0 (if everything went right) or 1 (if there was an 
+#      error
+#   file_id: string (identifier of the file, that can be used
+#      at /image?fileId={file_id})
+#   message: string (explanation of the error)
+# }
+# Uploads a file from the given URL and runs the face 
+# detection algorithm over it, saving the final composed image 
+# to the database.
+# If everything goes right, it returns the file id.
+# otherwise it returns an error message.
 def loadFromUrl():
+    # Masquerade as Mozilla because some web servers may not like python bots.
+    hdr = {'User-Agent': 'Mozilla/5.0'}
+    req = urllib2.Request(request.vars.url, headers=hdr)
     try:
-        url = request.get_vars["url"];
-        file = urllib2.urlopen(url)
-        return storeImage(file)
+        file = urllib2.urlopen(req)
+        return _storeImage(file)
 
     except urllib2.HTTPError as err:
         response.json({'status': 1, 'message': str(err)})
 
+# Endpoint: GET or POST /custom
+# Body: {
+#    image_title: string (title of the image)
+#    image_file: file (file to upload)
+#    color_hue: string (value of the hue variation between 0 and 360)
+#    color_sat: string (color saturation between 0.0 and 1.0)
+#    color_light: string (brightness between 0.0 and 1.0)
+# Response: HTML
+# If used with POST it will upload the given file, run face recogni-
+# tion and then apply color variations on the image, to finally store
+# it in the database.
+# In any case, it return the HTML page with the form to select and
+# trigger the upload.
 def custom():
 	image_form = FORM(
 		INPUT(_name='image_title',_type='text'),
@@ -81,30 +118,43 @@ def custom():
 		image.save(path)
 		id = db.image.insert(image=filename,title=image_form.vars.image_title)
 
-	images = db().select(db.image.ALL)
+	images = db().select(db.image.ALL, orderby=~db.image.createat)
 
 	return dict(images=images)
 
-def face_detection():
-    # Masquerade as Mozilla because some web servers may not like python bots.
-    hdr = {'User-Agent': 'Mozilla/5.0'}
-    # Set up the request
-    req = urllib2.Request(request.vars.url, headers=hdr)
+#--------------------------------------
+# Private methods
+#--------------------------------------
+
+# Runs face recognition and saves the final image, returning
+# JSON to the final user.
+def _storeImage(file):
     try:
-        # Obtain the content of the url
-        con = urllib2.urlopen(req)
-        output = generateImage(con)
-        output.save('image.png')
+        # Prune any images that are more than 1 hour old
+        _pruneDatabase(60)
+        # Generate new UUID for this image, based on our own domain
+        fileId = str(uuid.uuid5(uuid.NAMESPACE_DNS, 'got06.com'))
+        # Save the image as ".jpg" (it doesn't really matter the file
+        # extension, as the Image object knows how to convert formats
+        # anyways.
+        filename = db.image.image.store(file, fileId + ".jpg")
+        path = os.path.join(request.folder, 'uploads', filename)
+        # Generate and save the final image
+        image = _generateImage(open(path, 'rb'), 202, 0.15, 0.97, True)
+        image.save(path)
+        # Insert record in the database
+        db.image.insert(image=filename,title=fileId)
+        return response.json({'status': 0, 'file_id': fileId})
+    except ValueError as err:
+        return response.json({'status': 1, 'message': str(err)})
 
-        response.headers['Content-Type'] = "image/x-png"
-        response.headers['Content-disposition'] = 'attachment; filename=image.png'
-        res = response.stream(open('image.png', "rb"), chunk_size=4096)
-        return res
+# Trims the database, keeping only 1 day worth of images
+def _pruneDatabase(minutes):
+    limit = datetime.datetime.now.minusDay(minutes)
+    db(db.image.image == filename & db.image.createat < limit).delete()
 
-    except urllib2.HTTPError, e:
-        return e.fp.read()
-
-def rgb_to_hsv(rgb):
+# Converts RGB encoded images to HSV encoding
+def _rgb_to_hsv(rgb):
     # Translated from source of colorsys.rgb_to_hsv
     # r,g,b should be a numpy arrays with values between 0 and 255
     # rgb_to_hsv returns an array of floats between 0.0 and 1.0.
@@ -129,7 +179,8 @@ def rgb_to_hsv(rgb):
     hsv[..., 0] = (hsv[..., 0] / 6.0) % 1.0
     return hsv
 
-def hsv_to_rgb(hsv):
+# Converts HSV encoded images to RGB encoding
+def _hsv_to_rgb(hsv):
     # Translated from source of colorsys.hsv_to_rgb
     # h,s should be a numpy arrays with values between 0.0 and 1.0
     # v should be a numpy array with values between 0.0 and 255.0
@@ -149,14 +200,20 @@ def hsv_to_rgb(hsv):
     rgb[..., 2] = np.select(conditions, [v, p, t, v, v, q], default=p)
     return rgb.astype('uint8')
 
-def hueShift(img, amount):
+# Changes the hue of the image by converting it to HSV, applying an
+# offset to all values and then converting it back to RGB
+def _hueShift(img, amount):
     arr = np.array(img)
-    hsv = rgb_to_hsv(arr)
+    hsv = _rgb_to_hsv(arr)
     hsv[..., 0] = (hsv[..., 0]+amount) % 1.0
-    rgb = hsv_to_rgb(hsv)
+    rgb = _hsv_to_rgb(hsv)
     return Image.fromarray(rgb, 'RGB')
 
-def generateImage(con, hue, sat, light, fail):
+# Generates the final image, taking an image from the given connection
+# (con) and applying a hue, saturation and light adjustment.
+# It returns an Image object with the given result, or it throws a
+# ValueError if no face could be found or if there was any other error.
+def _generateImage(con, hue, sat, light, fail):
     # Module folder
     moduledir = os.path.dirname(os.path.abspath('__file__'))
     try:
@@ -201,7 +258,7 @@ def generateImage(con, hue, sat, light, fail):
             pilBlankImage = Image.fromarray(blankImage)
             pilBlankImage = ImageEnhance.Color(pilBlankImage).enhance(sat)
             pilBlankImage = ImageEnhance.Brightness(pilBlankImage).enhance(light)
-            pilBlankImage = hueShift(pilBlankImage, hue / 360.0)
+            pilBlankImage = _hueShift(pilBlankImage, hue / 360.0)
             pilBlankImage.paste(pilMaskImage, (0, 0), pilMaskImage)
             return pilBlankImage
         else:
@@ -218,3 +275,4 @@ def generateImage(con, hue, sat, light, fail):
 
     except urllib2.HTTPError, e:
         raise ValueError("Internal error processing the image")
+
